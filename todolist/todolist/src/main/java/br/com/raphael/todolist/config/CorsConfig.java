31a843354bf7645cfdebec8301ab24301bdb9746
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-08-08 08:39:25 -0300

    Update CorsConfig.java

--- todolist/todolist/src/main/java/br/com/raphael/todolist/config/CorsConfig.java
+++ todolist/todolist/src/main/java/br/com/raphael/todolist/config/CorsConfig.java
@@ -2,18 +2,17 @@ package br.com.raphael.todolist.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://todolist-api-production-ebdf.up.railway.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
