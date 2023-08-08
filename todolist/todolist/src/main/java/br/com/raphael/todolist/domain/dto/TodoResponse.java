665fb3b0bd77659a73cce729e14a88c681a3f8cd
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-08-08 14:56:06 -0300

    Update TodoResponse.java

--- todolist/todolist/src/main/java/br/com/raphael/todolist/domain/dto/TodoResponse.java
+++ todolist/todolist/src/main/java/br/com/raphael/todolist/domain/dto/TodoResponse.java
@@ -1,5 +1,7 @@
 package br.com.raphael.todolist.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public record TodoResponse(
@@ -7,7 +9,9 @@ public record TodoResponse(
         String title,
         String description,
         boolean concluded,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime creationDate,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime completionDate,
         int priority) {
 }
