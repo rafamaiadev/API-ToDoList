3ace7b3f1ec1d367ef2cdc2a803455edd7f3d422
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-08-10 14:08:26 -0300

    Create CorsConfig.java

--- todolist/todolist/src/main/java/br/com/raphael/todolist/config/CorsConfig.java
+++ todolist/todolist/src/main/java/br/com/raphael/todolist/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://todolist-api-production-ebdf.up.railway.app/swagger-ui/index.html")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
